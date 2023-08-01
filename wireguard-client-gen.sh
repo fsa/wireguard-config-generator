46a65cccb4efa7e65aaf01c31685365b97c4aca5
Sergei Fedotov <fsa@tavda.info>
2023-08-02 04:00:56 +0700

    Генерация разделена на два скрипта

---
+++ wireguard-client-gen.sh
@@ -0,0 +1,53 @@
+if [ -f "wgconfigs/.env" ]
+then
+    source wgconfigs/.env
+else
+    echo "Wireguard Server config not found";
+    exit 1;
+fi
+
+# Вспомогательная функция для герерации IPv6 адресов
+IPV6_ARRAY=( 1 2 3 4 5 6 7 8 9 0 a b c d e f )
+gen_block () {
+    echo ${IPV6_ARRAY[$RANDOM%16]}${IPV6_ARRAY[$RANDOM%16]}${IPV6_ARRAY[$RANDOM%16]}${IPV6_ARRAY[$RANDOM%16]}
+}
+
+read CLIENT_IP_SUFFIX < wgconfigs/client_ip_suffix.txt
+if [ -z "$CLIENT_IP_SUFFIX" ]; then
+    CLIENT_IP_SUFFIX=2
+fi
+CLIENT_IP6_SUFFIX=$(gen_block):$(gen_block):$(gen_block):$(gen_block);
+mkdir -p wgconfigs/clientconfigs
+
+echo generating client config w/ IP ${WG_IPV4_PREFIX}${CLIENT_IP_SUFFIX}, ${WG_IPV6_PREFIX}${CLIENT_IP6_SUFFIX}
+
+WG_CLIENT_PRIVATE_KEY=$(wg genkey)
+WG_CLIENT_PUBLIC_KEY=$(echo "$WG_CLIENT_PRIVATE_KEY" | wg pubkey)
+WG_CLIENT_PSK=$(wg genpsk)
+
+cat > wgconfigs/clientconfigs/${WG_SERVER_INTERFACE}c${CLIENT_IP_SUFFIX}.conf << EOF 
+[Interface]
+Address = ${WG_IPV4_PREFIX}${CLIENT_IP_SUFFIX}/32,${WG_IPV6_PREFIX}${CLIENT_IP6_SUFFIX}/128
+PrivateKey = ${WG_CLIENT_PRIVATE_KEY}
+DNS = ${DNS}
+
+[Peer]
+PublicKey = ${WG_SERVER_PUBLIC_KEY}
+PresharedKey = ${WG_CLIENT_PSK}
+Endpoint = ${WG_SERVER_ADDRESS}:${WG_SERVER_PORT}
+AllowedIPs = ${WG_CLIENT_ALLOWED_IPS}
+EOF
+
+# add a peer to server config
+cat >> wgconfigs/${WG_SERVER_INTERFACE}.conf << EOF 
+
+[Peer]
+PublicKey = ${WG_CLIENT_PUBLIC_KEY}
+PresharedKey = ${WG_CLIENT_PSK}
+AllowedIPs = ${WG_IPV4_PREFIX}${CLIENT_IP_SUFFIX}/32,${WG_IPV6_PREFIX}${CLIENT_IP6_SUFFIX}/128
+EOF
+
+CLIENT_IP_SUFFIX=$[$CLIENT_IP_SUFFIX+1]
+echo $CLIENT_IP_SUFFIX > wgconfigs/client_ip_suffix.txt
+
+exit 0
---
+++ wireguard-server-gen.sh
@@ -0,0 +1,69 @@
+#!/usr/bin/env bash
+# SPDX-License-Identifier: GPL-3.0-only
+
+WG_INTERFACE=wg0
+
+DNS="1.1.1.1 1.0.0.1"
+
+WG_CLIENT_ALLOWED_IPS="1.0.0.0/8, 2.0.0.0/7, 4.0.0.0/6, 8.0.0.0/7, 11.0.0.0/8, 12.0.0.0/6, 16.0.0.0/4, 32.0.0.0/3, 64.0.0.0/3, 96.0.0.0/4, 112.0.0.0/5, 120.0.0.0/6, 124.0.0.0/7, 126.0.0.0/8, 128.0.0.0/3, 160.0.0.0/5, 168.0.0.0/8, 169.0.0.0/9, 169.128.0.0/10, 169.192.0.0/11, 169.224.0.0/12, 169.240.0.0/13, 169.248.0.0/14, 169.252.0.0/15, 169.255.0.0/16, 170.0.0.0/7, 172.0.0.0/12, 172.32.0.0/11, 172.64.0.0/10, 172.128.0.0/9, 173.0.0.0/8, 174.0.0.0/7, 176.0.0.0/4, 192.0.0.0/9, 192.128.0.0/11, 192.160.0.0/13, 192.169.0.0/16, 192.170.0.0/15, 192.172.0.0/14, 192.176.0.0/12, 192.192.0.0/10, 193.0.0.0/8, 194.0.0.0/7, 196.0.0.0/6, 200.0.0.0/5, 208.0.0.0/4, 224.0.0.0/4, 2000::/3"
+
+if [ -f ".env" ]; then source .env; fi
+
+if [ -d wgconfigs ]
+then
+    echo "Конифгурация сервера уже существует. Удалите директорию wgconfigs, чтобы создать новую конфигурацию!"
+    exit 1
+fi
+mkdir wgconfigs
+
+# Вспомогательная функция для герерации IPv6 адресов
+IPV6_ARRAY=( 1 2 3 4 5 6 7 8 9 0 a b c d e f )
+gen_block () {
+    echo ${IPV6_ARRAY[$RANDOM%16]}${IPV6_ARRAY[$RANDOM%16]}${IPV6_ARRAY[$RANDOM%16]}${IPV6_ARRAY[$RANDOM%16]}
+}
+
+echo generating server config
+# generate the [Interface] part for the server
+if [ -z "$WG_SERVER_ADDRESS" ]; then
+    echo "Не задан адрес сервера"
+    exit 2
+fi
+if [ -z "$PUBLIC_NETWORK_INTERFACE" ]; then
+    echo "Не задан публичный интерфейс сервера"
+    exit 3
+fi
+if [ -z "$WG_IPV4_PREFIX" ]; then
+    WG_IPV4_PREFIX=10.$(shuf -i1-254 -n1).$(shuf -i1-254 -n1).
+fi
+if [ -z "$WG_IPV6_PREFIX" ]; then
+    WG_IPV6_PREFIX=fd00:$(gen_block):$(gen_block):$(gen_block):
+fi
+if [ -z "$WG_SERVER_PORT" ]; then
+    WG_SERVER_PORT=$(shuf -i10000-65535 -n1)
+fi
+WG_SERVER_PRIVATE_KEY=$(wg genkey)
+WG_SERVER_PUBLIC_KEY=$(echo "$WG_SERVER_PRIVATE_KEY" | wg pubkey)
+
+cat > wgconfigs/.env << EOF
+WG_IPV4_PREFIX=${WG_IPV4_PREFIX}
+WG_IPV6_PREFIX=${WG_IPV6_PREFIX}
+WG_SERVER_PUBLIC_KEY=${WG_SERVER_PUBLIC_KEY}
+WG_SERVER_ADDRESS=${WG_SERVER_ADDRESS}
+WG_SERVER_PORT=${WG_SERVER_PORT}
+WG_SERVER_INTERFACE=${WG_INTERFACE}
+WG_CLIENT_ALLOWED_IPS="${WG_CLIENT_ALLOWED_IPS}"
+DNS="${DNS}"
+EOF
+
+cat > wgconfigs/${WG_INTERFACE}.conf << EOF 
+[Interface]
+Address = ${WG_IPV4_PREFIX}1/24
+Address = ${WG_IPV6_PREFIX}1/64
+SaveConfig = true
+PostUp = iptables -A FORWARD -i %i -j ACCEPT -w 10; iptables -t nat -A POSTROUTING -o ${PUBLIC_NETWORK_INTERFACE} -j MASQUERADE -w 10
+PostDown = iptables -D FORWARD -i %i -j ACCEPT; iptables -t nat -D POSTROUTING -o ${PUBLIC_NETWORK_INTERFACE} -j MASQUERADE
+ListenPort = ${WG_SERVER_PORT}
+PrivateKey = ${WG_SERVER_PRIVATE_KEY}
+EOF
+
+exit 0
